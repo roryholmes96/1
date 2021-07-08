6db8b20be2fd538743eb65deecd4d6589d3e4f70
roryholmes96 <86110630+roryholmes96@users.noreply.github.com>
2021-07-08 09:53:59 +0100

    Update rim_weighting_algorithm.py

--- rim_weighting_algorithm.py
+++ rim_weighting_algorithm.py
@@ -1,7 +1,9 @@
 import pprint, math as m, pyodbc
 
 def calcFrequencies(responses):
-    '''Calculates frequencies for responses in a given 'column' '''
+    '''Calculates the frequencies for each possible responses in a survey 
+       variable's list of responses
+    '''
     frequencies = {}
 
     for response in responses:
@@ -13,11 +15,17 @@ def calcFrequencies(responses):
     return frequencies
 
 def crosstab(*responses):
-	'''crosstabs columns of responses'''
+	'''Crosstabs the lists of responses for multiple survey variables. The
+	   crosstab is a dictionary where there is a key for each possible 
+	   combination of responses with the frequency of cases who responded this 
+	   way as the value.
+	'''
 	return calcFrequencies(list(zip(*responses)))
 
 def aggregateCrosstabFrequencies(index):
-	'''Aggregates frequencies of responses for each 'column' in a crosstab'''
+	'''Aggregates the frequencies of responses for each survey variable in a 
+	   crosstab.
+	'''
 	frequencies = {}
 
 	for t, frequency in currentCrosstab.items():
@@ -29,8 +37,11 @@ def aggregateCrosstabFrequencies(index):
 
 	return dict(sorted(frequencies.items()))
 
-def frequenciesDifference(currentFrequencies, desiredFrequencies):
-	'''Calculates total difference between current and desired frequencies'''
+def frequenciesDiff(currentFrequencies, desiredFrequencies):
+	'''Calculates the difference between current frequencies (i.e. the 
+	   frequencies after current iteration of weighting) and desired frequencies 
+	   for each possible response in a survey variable's responses
+	'''
 	totalDiff = 0
 
 	for currentFrequency, desiredFrequency in\
@@ -39,15 +50,17 @@ def frequenciesDifference(currentFrequencies, desiredFrequencies):
 
 	return totalDiff
 
-def calcDifferencePerCase(currentFrequencies, desiredFrequencies):
-	'''Calculates total difference between current and desired frequencies 
-	per case (this is used to check whether to perform another iteration)'''
-	difference = frequenciesDifference(currentFrequencies, desiredFrequencies)
+def diffPerCase(currentFrequencies, desiredFrequencies):
+	'''Calculates the total difference between current and desired frequencies 
+	   for a given survey variable, divided by the number of respondents 
+	   (this is used to check whether to perform another iteration)
+	'''
+	difference = frequenciesDiff(currentFrequencies, desiredFrequencies)
 	sampleSize = sum(actualCrosstab.values())
 	return difference / sampleSize
 
 def calcWeights(currentFrequencies, desiredFrequencies):
-	'''Calculates weight factors as df/cf quotient'''
+	'''Calculates weights as desiredFrequencies/currentFrequencies quotient '''
 	weights = []
 
 	for currentFrequency, desiredFrequency in\
@@ -57,16 +70,26 @@ def calcWeights(currentFrequencies, desiredFrequencies):
 	return weights
 
 def applyWeights(index):
-	'''applies weights to 'column'(index in tuple) in current crosstab'''
+	'''Applies weights to the (current) frequencies of a given variable in the
+	   current crosstab (i.e. the crosstab after current iteration of 
+	   weighting). The index passed as an argument denotes the position
+	   of the survey  variable in the current crosstab's key tuple. For each 
+	   item in the crosstab, this formula retrieves the value of the response 
+	   (e.g. 2 for gender 2). It then uses this value minus 1 as the index for
+	   the corresponding weight in the weights list. This is because there is a
+	   weight for each possible reponse in the weights list stored in order. The
+	   minus 1 is because the index starts at 0. (e.g. the weight for gender 2 
+	   is stored in index 1 in the weights list)
+	'''
 	for k, v in currentCrosstab.items():
 		v = v*weights[int(k[index])-1]
 		currentCrosstab[k] = v
 
-class RimColumn():
-	''' Represents columns to be used in RIM Weighting'''
-	def __init__ (self, column, desiredFrequencies, index):
+class RimVariable():
+	''' Represents survey variables to be used in RIM Weighting'''
+	def __init__ (self, responses, desiredFrequencies, index):
 		self.index = index
-		self.actualFrequencies = calcFrequencies(column)
+		self.actualFrequencies = calcFrequencies(surveyVariableResponses)
 		self.currentFrequencies = self.actualFrequencies
 		self.desiredFrequencies = desiredFrequencies
 
@@ -83,27 +106,28 @@ data = []
 for row in cursor:
 	data.append(row[6])
 
-#Creates list for each 'column'
-genders = []
-ages = []
-ageBins = []
-deodorantUses = []
+#Creates list for the set of responses for each survey variable
+genderResponses = []
+ageResponses = []
+ageResponsesBins = []
+deodorantUseResponses = []
 
-#Separates data by 'column' and appends these to respective 'column' lists
+#Separates data by survey variable and appends these to respective survey
+#variable response lists
 for i in data:
 	x = i.split('&')
 
 	for i in x:
 		if 'Qs0' in i:
-			genders.append(i.replace('Qs0=',''))
+			genderResponses.append(i.replace('Qs0=',''))
 		elif 'Qs1_1' in i:
-			ages.append(int(i.replace('Qs1_1=','')))
+			ageResponses.append(int(i.replace('Qs1_1=','')))
 		elif 'Qs3' in i:
-			deodorantUses.append(i.replace('Qs3=',''))
+			deodorantUseResponses.append(i.replace('Qs3=',''))
 
-#Splits age data into age bins
-for i in range(len(ages)):
-    ageBins.append(m.floor((ages[i]-1)/10))
+#Splits age data into bins
+for i in range(len(ageResponses)):
+    ageResponsesBins.append(m.floor((ageResponses[i]-1)/10))
 
 #Arbitrary desired frequency distributions
 desiredAgeFrequencies = \
@@ -112,34 +136,43 @@ desiredAgeFrequencies = \
 desiredGenderFrequencies = \
 {'1':700, '2':805}
 
-#Creates columns list which contains each column of responses and desired frequencies list
-columns = [ageBins, genders]
+#Creates list which contains the list of responses for each survey variable to
+#be used in rim-weighting
+surveyVariableResponses = [ageResponsesBins, genderResponses]
+
+#Creates list which contains the frequency distribution dictonary for each 
+#survey variable to be used in rim-weightin
 desiredFrequencies = [desiredAgeFrequencies, desiredGenderFrequencies]
 
-#Creates index list which contains the index (to be used in various functions) for each item in columns list
+#Creates index list which contains the index (to be used in various functions) 
+#for each item in surveyVariableResponses list
 index = []
-for t in list(enumerate(columns)):
+for t in list(enumerate(surveyVariableResponses)):
 	index.append(t[0])
 
-#Creates a structure to be used in loop which assigns objects to Rim Column class
-#Item in structure is a tuple containing survey variable's column of data, the desired frequency distribution for that variable, and its index position (this is the same in both this structure and the crosstab)
-structure = list(zip(columns, desiredFrequencies, index))
+#Creates a structure to be used in loop which assigns objects to RimVariable
+#class. Each item in the structure is a tuple containing survey variable's list
+#of responses, the desired frequency distribution for that variable, and its 
+#index position (this is the same in both this structure and the crosstab)
+structure = list(zip(surveyVariableResponses, desiredFrequencies, index))
 
-#Assigns actual and an initial current crosstab.
-actualCrosstab = crosstab(*columns)
-currentCrosstab = crosstab(*columns)
+#Assigns actual and an initial current crosstab
+actualCrosstab = crosstab(*surveyVariableResponses)
+currentCrosstab = crosstab(*surveyVariableResponses)
 
-#Creates Rim Columns to be used in rim-weighting algorithm
-rimColumns = []
-for column, desiredFrequencies, index in structure:
-	rimColumns.append(RimColumn(column, desiredFrequencies, index))
+#Creates a RimVariable instance for each variable to be used in the
+#rim-weighting algorithm and appends it to rimVariables list
+rimVariables = []
+for surveyVariableResponses, desiredFrequencies, index in structure:
+	rimVariable =RimVariable(surveyVariableResponses, desiredFrequencies, index)
+	rimVariables.append(rimVariable)
 
-#Assigns initial total difference between actual and desired freq per 
+#Assigns initial total difference between actual and desired freq per case
 totalDiffPerCase = 0
-for rimColumn in rimColumns:
+for rimVariable in rimVariables:
 	totalDiffPerCase += \
-	calcDifferencePerCase(rimColumn.currentFrequencies,\
-						  rimColumn.desiredFrequencies)
+	diffPerCase(rimVariable.currentFrequencies,\
+				rimVariable.desiredFrequencies)
 
 # RIM-Weighting Algorithm
 iteration = 0
@@ -147,22 +180,23 @@ while totalDiffPerCase > 0.00000000000000001:
 	totalDiffPerCase = 0
 
 	#Calculates and applies weights to crosstab frequencies
-	for rimColumn in rimColumns:
+	for rimVariable in rimVariables:
 		weights = \
-		calcWeights(rimColumn.currentFrequencies,rimColumn.desiredFrequencies)
+		calcWeights(rimVariable.currentFrequencies,\
+					rimVariable.desiredFrequencies)
 
-		applyWeights(rimColumn.index)
+		applyWeights(rimVariable.index)
 
 		#Updates current freq to reflect newly weighted crosstab freq
-		for rimColumn in rimColumns:
-			rimColumn.currentFrequencies =\
-			aggregateCrosstabFrequencies(rimColumn.index)
+		for rimVariable in rimVariables:
+			rimVariable.currentFrequencies =\
+			aggregateCrosstabFrequencies(rimVariable.index)
 
 	#Calculates total difference per case at end of iteration
-	for rimColumn in rimColumns:
+	for rimVariable in rimVariables:
 		totalDiffPerCase += \
-		calcDifferencePerCase(rimColumn.currentFrequencies,\
-							  rimColumn.desiredFrequencies)
+		diffPerCase(rimVariable.currentFrequencies,\
+							  rimVariable.desiredFrequencies)
 		
 	#Increases iteration and prints current iteration and difference per case
 	iteration +=1
