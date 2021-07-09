c9eaf87af1d20bbc5142b832ad6a2bc1e8568a0a
roryholmes96 <86110630+roryholmes96@users.noreply.github.com>
2021-07-09 09:48:33 +0100

    Update CSV_to_SQL.py

--- CSV_to_SQL.py
+++ CSV_to_SQL.py
@@ -8,33 +8,37 @@ with open(filename,encoding='utf-8-sig') as f:
     headers = next(reader)
     reader = list(reader)
 
-    connection = pyodbc.connect('Driver={SQL Server};'
-					        'Server=.\sqlexpress;'
-					        'Database=Pokerstars;'
-					        'Trusted_connection=yes')
-    try:
-	    for i in range(9999999999999999999999):
-	    	varResPairs=list(zip(headers, reader[i]))
-	    	rowString = ''
-	    	field = 1
-	    	for variable, response in varResPairs:
-	    		if field == 1:
-	    			rowString+=(f'{variable}={response}')
-	    			field +=1
-	    		else:
-	    			rowString+=(f'&{variable}={response}')
+connection = pyodbc.connect('Driver={SQL Server};'
+				        'Server=.\sqlexpress;'
+				        'Database=Pokerstars;'
+				        'Trusted_connection=yes')
 
-	    	try:
-	    		SQL_insert_query = """INSERT INTO pokerstars (Data) VALUES ('%s') """ % rowString
+cursor = connection.cursor()
 
-	    		cursor = connection.cursor()
-	    		cursor.execute(SQL_insert_query)
-	    		connection.commit()
-	    		print(cursor.rowcount, "Row inserted successfully into table")
-	    		cursor.close()
+try:
+    for i in range(9999999999999999999999):
+    	varResPairs=list(zip(headers, reader[i]))
+    	rowString = ''
+    	field = 1
+    	for variable, response in varResPairs:
+    		if field == 1:
+    			rowString+=(f'{variable}={response}')
+    			field +=1
+    		else:
+    			rowString+=(f'&{variable}={response}')
 
-	    	except pyodbc.Error as error:
-	    		print("Failed to insert row into table {}".format(error))
+    	try:
+    		SQL_insert_query = """INSERT INTO pokerstars (Data) VALUES ('%s') """ % rowString
 
-    except IndexError as error:
-	    print('No more rows left to insert')
+    		cursor.execute(SQL_insert_query)
+    		connection.commit()
+    		print(cursor.rowcount, "Row inserted successfully into table")
+
+    	except pyodbc.Error as error:
+    		print("Failed to insert row into table {}".format(error))
+
+except IndexError as error:
+    print('No more rows left to insert')
+
+cursor.close()
+connection.close()
