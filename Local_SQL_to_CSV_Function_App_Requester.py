58b5b3544a610ad506aef9e74b0aa59af96115af
roryholmes96 <86110630+roryholmes96@users.noreply.github.com>
2021-07-15 15:28:12 +0100

    Update Local_SQL_to_CSV_Function_App_Requester.py

--- Local_SQL_to_CSV_Function_App_Requester.py
+++ Local_SQL_to_CSV_Function_App_Requester.py
@@ -3,18 +3,24 @@ some part of data from SQL, converts it to CSV format and returns this as a
 string. This program retrieves the strings in parallel and then writes them to 
 a CSV file.'''
 
-#Modifiable Inputs and paramaters
-numberOfThreads = 20
+#Modifiable Inputs
+numberOfThreads = 3
 headersFile = 'pokerstars_headers.csv'
 newFile = 'pokerstars_data.csv'
 table = 'pokerstars'
 
 import pyodbc, csv, requests, json, requests
 from requests_futures.sessions import FuturesSession
+import time
+from datetime import timedelta
+start_time = time.monotonic()
 
 def writeToFile(resp, *args, **kwargs):
 	'''writes responses to file in order the requests are finished'''
-	newFile.write(resp.text.replace('\n',''))
+	end_time = time.monotonic()
+	print(timedelta(seconds=end_time - start_time))
+	print(resp)
+	# newFile.write(resp.text.replace('\n',''))
 
 #Creates a list of headers from CSV file containing headers
 #then converts this list to JSON.
@@ -24,13 +30,12 @@ with open(headersFile, encoding='utf-8-sig') as f:
 
 jsonHeaders = json.dumps(headers)
 
-#Opens the file which the CSV data will be written to.
-newFile = open(newFile,'a')
+##Opens the file which the CSV data will be written to.
+# newFile = open(newFile,'a')
 
 #Sends a request to a function app which returns the number of database rows
 numberOfRowsRequest = requests.get(\
-f'https://sql-to-csv.azurewebsites.net/api/HttpTrigger_Get_Number_of_Database_Rows?\
-table={table}')
+	f'https://sql-to-csv.azurewebsites.net/api/HttpTrigger_Get_Number_of_Database_Rows?table={table}')
 
 numberOfRows = int(numberOfRowsRequest.text)
 
@@ -52,9 +57,10 @@ session.hooks['response'] = writeToFile
 
 for rowBatch in rowBatches:
 	params = {'table': table, 'firstRow':rowBatch[0], 'lastRow':rowBatch[1]}
-	future = session.get('https://sql-to-csv.azurewebsites.net/api/HttpTrigger_SQL_to_CSV',\
-						  data=jsonHeaders, params=params)
+	future = session.get(\
+		'https://sql-to-csv.azurewebsites.net/api/HttpTrigger_SQL_to_CSV',\
+		data=jsonHeaders, params=params)
 
-#Writes headers into new file whilst requests are being completed
-writer = csv.DictWriter(newFile, fieldnames=headers)
-writer.writeheader()
+##Writes headers into new file whilst requests are being completed
+# writer = csv.DictWriter(newFile, fieldnames=headers)
+# writer.writeheader()
